05d4143a7344b2651c22e361dba42da8b293d072
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-08-23 00:01:30 +0900

    新しいViewとテキストを追加した

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
