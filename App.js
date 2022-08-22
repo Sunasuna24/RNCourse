b4079fbdf77c0f7d0b8f6784050567071681065b
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-08-23 00:07:18 +0900

    インラインでCSSを適用した

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
       <View>
         <Text>Another piece of text!</Text>
       </View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{margin: 16, borderWidth: 2, borderColor: 'red', padding: 16}}>Open up App.js to start working on your app!</Text>
       <Button title="click me!" />
     </View>
   );
