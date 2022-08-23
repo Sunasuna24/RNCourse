8750b344acb83c958c61b107f18d8fe6967425d2
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-08-23 23:38:33 +0900

    ScrollViewの代わりにFlatListを導入してリストを非同期的に読み込むようにした

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState(''); // テキストを保存するからuseStateの初期値を「''」にする
@@ -10,7 +10,7 @@ export default function App() {
   };
 
   function addGoalHandler() {
-    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]); // 配列の中身を更新する方法、ドキュメントなどで推奨されているやり方らしい
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, {text: enteredGoalText, key: Math.random().toString()}]); // 配列の中身を更新する方法、ドキュメントなどで推奨されているやり方らしい
   };
 
   return (
@@ -26,13 +26,19 @@ export default function App() {
         />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView>
-          {courseGoals.map((goal) => (
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
       </View>
     </View>
   );
